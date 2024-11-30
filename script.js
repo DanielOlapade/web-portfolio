fc6ccd5c1e0bdeb567a313c3dfc1a84570d8e4fe
Daniel <olapadedaniel99@gmail.com>
2024-11-30 06:00:39 -0500

    create 2 files and update 3 files

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const resetpage =document.getElementById("resetpage");
+const content =document.getElementsByClassName("content");
 
 resetpage.addEventListener('click', () => {
     window.location.reload();
 });
+
+for (let i = 0; i < content.length; i++) {
+    content[i].addEventListener("mouseover", () => {
+        content[i].play();
+        content[i].loop = true;
+        content[i].muted = true;
+    });
+    content[i].addEventListener("mouseout", () => {
+        content[i].pause();
+        content[i].currentTime = 0;
+    });
+}
+
+
+
 
 
