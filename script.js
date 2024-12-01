92e08e9ba289a90bab80acfce621519d77351cf4
Daniel <olapadedaniel99@gmail.com>
2024-12-01 09:50:01 -0500

    create 3 files, delete 1 file and update 3 files update script.js

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const resetpage =document.getElementById("resetpage");
-const content =document.getElementsByClassName("content");
-
 resetpage.addEventListener('click', () => {
     window.location.reload();
 });
 
-for (let i = 0; i < content.length; i++) {
-    content[i].addEventListener("mouseover", () => {
-        content[i].play();
-        content[i].loop = true;
-        content[i].muted = true;
-    });
-    content[i].addEventListener("mouseout", () => {
-        content[i].pause();
-        content[i].currentTime = 0;
-    });
-}
+const content = document.getElementsByClassName("content")
+for (let i=0; i < content.length; i++){
+    if (content[i].tagName === "VIDEO"){
+        content[i].addEventListener("mouseover", ()=>{
+            content[i].play();
+            content[i].loop();
+            content[i].muted();
+        });
+        content[i].addEventListener("mouseleave", ()=>{
+            content[i].pause();
+            content[i].currentTime = 0;
+        });
+    };
+};
+
+const theme = document.getElementById("theme");
+theme.addEventListener("click", () => {
+    document.body.classList.toggle("lightmode");
+    if (document.body.classList.contains("lightmode")) {
+        sessionStorage.setItem("theme", "light");
+    } else {
+        sessionStorage.setItem("theme", "dark");
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const theme = sessionStorage.getItem("theme");
+    if (theme === "light") document.body.classList.add("lightmode");
+});
 
 
 
